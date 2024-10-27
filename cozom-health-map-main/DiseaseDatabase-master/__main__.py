40d49981017ba361276dd293cbeb0ca96c719b51
cozom <mauryasatish6@gmail.com>
2024-10-27 13:05:58 +0530

    Updated __main__.py to conditionally use GUI based on environment

--- cozom-health-map-main/DiseaseDatabase-master/__main__.py
+++ cozom-health-map-main/DiseaseDatabase-master/__main__.py
@@ -1,14 +1,23 @@
 import sys
-from GUI.About import About
-from GUI.Diagnose import Diagnose
-from PyQt5.QtWidgets import QApplication
+import os
+
 from WebMD.reader import readDatabase
 
+# Check if the server environment is set
+if os.environ.get('SERVER_ENV') is None:
+    from PyQt5.QtWidgets import QApplication
+    from GUI.About import About
+    from GUI.Diagnose import Diagnose
 
-if __name__ == '__main__':
-    database = readDatabase('./data')
-    app = QApplication(sys.argv)
-    diagnose = Diagnose(database)
-    about = About(diagnose)
-    about.show()
-    sys.exit(app.exec_())
+    # Local Development
+    if __name__ == '__main__':
+        database = readDatabase('./data')
+        app = QApplication(sys.argv)
+        diagnose = Diagnose(database)
+        about = About(diagnose)
+        about.show()
+        sys.exit(app.exec_())
+else:
+    # Server Mode
+    print("Running in server mode - GUI components disabled")
+    # You can add server-specific logic here
